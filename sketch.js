319e2505e25a6a8ae80b3823f4113a86a7a3b7d7
jonaspavan <pavan.costa@escola.pr.gov.br>
2024-09-23 15:32:14 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("black");
+  fill("white")
+  ellipse(200, 200, 125, 125 )
+  stroke("white")
+  ellipse(200, 300, 170, 170 )
+  stroke("white")
+  ellipse(200, 108, 90, 90 )
+  stroke("white")
+  fill("black")
+  ellipse(185, 101, 20, 20)
+  fill("black")
+  ellipse(215, 101, 20, 20)
+  fill("red")
+  ellipse(185, 101, 4, 4)
+  fill("red")
+  ellipse(215, 101, 4, 4)
+  fill("orange")
+  triangle(160, 122, 200, 115, 200, 125)
+  fill("black")
+  ellipse(200, 135, 40, 20)
+  fill("white")
+  rect(180, 125, 40, 10)
+  fill("white")
+  triangle(190, 135, 195, 135, 193, 142 )
+  triangle(196, 135, 201, 135, 199, 143 )
+  triangle(202, 135, 207, 135, 205, 142 )
+  triangle(208, 135, 213, 135, 211, 141 )
+  triangle(189, 135, 184, 135, 187, 141 )
+  stroke("black")
+  line(150, 170, 180, 230)
+  line(250, 170, 225, 230)
+  square(180, 230, 50 )
+  
+
+}
